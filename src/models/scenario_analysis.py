cb90949aa1f2e4e57cf0c9b0ac01f06c84c8e57b
Bitania Finoteselam <hanafinoteselam.15@gmail.com>
2026-07-29 21:25:44 +0300

    Create scenario_analysis.py

---
+++ src/models/scenario_analysis.py
@@ -0,0 +1,9 @@
+def generate_scenario_forecast(
+    base_forecast,
+    historical_growth,
+    growth_adjustment
+):
+    adjusted_growth = historical_growth + growth_adjustment
+
+    # Apply adjusted growth to future periods
+    ...
